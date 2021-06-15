aa1c79034de75bbdb353c31733a56ad79e31b320
Zachary Williams <kcaz311@gmail.com>
2021-06-15 10:47:50 -0400

    transaction show working

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,10 +1,19 @@
 import React from 'react' 
 
+const depositOrWithdraw = (kind) => {
+    if (kind)
+      return "deposit"
+    else
+      return "withdraw"
+    }
+
 const Transactions = (props) => {
     
     return (
         <div>
-            Transactions
+            {props.transactions && props.transactions.map(transaction => 
+                <li key={transaction.id}>{transaction.amount} - {depositOrWithdraw(transaction.kind)} - {transaction.date}</li>
+                )}
         </div>
     )
 }
